af73c5b52311e21b8e27d43ebaca66f8628ac5dc
maherfauzi <azirariza9@gmail.com>
2025-04-02 07:42:39 +0700

    [update] build.sh

--- README.md
+++ README.md
@@ -3,3 +3,12 @@
 ## Description 
 Rewrite of my vehicle counter program in C++ (previously in python), this program was used for my final project in college
 
+## Usage
+just run 
+```sh
+./build.sh && ./vehicleCounter
+```
+
+actually using build.zig for build system but i dont why but it didnt want to detect libopencv_imgcodecs, so i ditch it for good old shell
+
+
--- build.sh
+++ build.sh
@@ -1,2 +1,28 @@
 #!/bin/bash
- clang++ -v main.cpp -o vehicleCounter -Wl,-rpath,./lib64 -I. -I./include -L./lib64/ -lopencv_core -lopencv_imgcodecs -lopencv_highgui -lopencv_imgproc
+
+CXX="clang++"
+CXXFLAGS="-v"
+SRC="main.cpp"
+OUTPUT="vehicleCounter"
+
+INCLUDES="-I. -I./include"
+
+LIBDIR="./lib64"
+LIB_PATH="-L${LIBDIR}"
+RPATH="-Wl,-rpath,${LIBDIR}"
+
+LIBS="-lopencv_core -lopencv_imgcodecs -lopencv_highgui"
+
+build_app() {
+    echo "Building ${OUTPUT} with source ${SRC}"
+    ${CXX} ${CXXFLAGS} ${SRC} -o ${OUTPUT} ${RPATH} ${INCLUDES} ${LIB_PATH} ${LIBS}
+}
+
+build_app
+
+if [ $? -eq 0 ]; then
+    echo "Build succeeded: ${OUTPUT} created."
+else
+    echo "Build failed." >&2
+    exit 1
+fi
